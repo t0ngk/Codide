b194862b517de063c025447017568a1fa98b34f5
T0NG.K <39180519+t0ngk@users.noreply.github.com>
2024-05-10 23:48:31 +0700

    🚀 : Vercel

--- src/lib/components/navbar/Navbar.svelte
+++ src/lib/components/navbar/Navbar.svelte
@@ -2,7 +2,7 @@
 	class="p-4 w-full h-full bg-[#080404] border-b border-[#393939] text-white flex items-center gap-2 justify-between"
 >
 	<div class="flex gap-2 items-center">
-		<h3 class="text-2xl">Codide x TobeIT</h3>
+		<h3 class="text-2xl">Codide</h3>
     <slot name="title" />
 	</div>
   <slot name="action" />
